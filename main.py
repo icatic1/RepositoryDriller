d7fe76c566a490baaf61ab7bf72d15bd390cbb03
Irvin Catic <icatic1@etf.unsa.ba>
2022-04-29 17:21:23 +0200

    Add englishCommit function

--- main.py
+++ main.py
@@ -44,6 +44,50 @@ def modifiedFilesPerCommit(commit, stringFileName):
 
     return False
 
+def englishCommit(repoString):
+    full = 0
+    length = 0
+    startingWords = ["Initial", "Create", "Fix", "Add", "Remove", "Change", "Clear", "Initialize", "Merge", "Submit",
+                     "Handle", "Update", "Set", "Move", "Force", "Expand", "Edit", "Check", "Modify", "Bump", "Refactor"]
+    commits = Repository(repoString).traverse_commits()
+    for commit in commits:
+        send = 1
+        message = commit.msg
+        words = message.split(" ")
+        print(words)
+        if words[0].endswith("ing") or words[0].endswith("ed"):
+            print("Wrong1")
+            send -= 0.15
+        if words[0][0].islower():
+            print("Wrong2")
+            send -= 0.15
+        if not words[0] in startingWords:
+            print("Wrong3")
+            send -= 0.25
+        if words[len(words) - 1].endswith("."):
+            print("Wrong4")
+            send -= 0.1
+        if len(message) >= 50:
+            print("Wrong5")
+            send -= 0.25
+        containsFile = False
+
+        for w in words:
+            if w != "Initial" and w != "Initialize":
+                if modifiedFilesPerCommit(commit, w):
+                    containsFile = True
+            else:
+                containsFile = True
+
+        if not containsFile:
+            print("Wrong6")
+            send -= 0.1
+
+        full += send
+        length+=1
+
+    return full/length
+
 def evaluationDMM(repoString):
     rm = Repository(repoString)
     allunit = 0
@@ -79,4 +123,14 @@ def evaluationDMM(repoString):
 
 
 if __name__ == '__main__':
-    listOfRepositories()
+    print('Number of arguments:', len(sys.argv), 'arguments.')
+    print('Argument List:', str(sys.argv))
+    # getAllrepos(sys.argv[1])
+    # DMM_ocjena("https://github.com/icatic1/Promnesia")
+    # englishCommit("https://github.com/vljubovic/c9etf")
+    # DMM_ocjena("https://github.com/vljubovic/c9etf")
+    a = englishCommit("https://github.com/adzaferbeg1/AuctionApp")
+    b = evaluationDMM("https://github.com/adzaferbeg1/AuctionApp")
+    print(a)
+    print(b)
+    print(a * 0.75 + b * 0.25)
