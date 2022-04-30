a05e61f2b0b901c44d54bbf16c1bb26a7ff0706e
Irvin Catic <icatic1@etf.unsa.ba>
2022-04-30 14:50:40 +0200

    Add bosnianCommit and support for second language

--- main.py
+++ main.py
@@ -21,9 +21,13 @@ def getAllRepositories(stringFolder, language):
             for commit in r.traverse_commits():
                 author = commit.author.name
                 break
+            eComm = None
             eDMM = evaluationDMM(rep)
-            eComm = englishCommit(rep)
-            print(rep + " " + author)
+            if language == "-b":
+                eComm = bosnianCommit(rep)
+            else:
+                eComm = englishCommit(rep)
+            print(rep + " -- " + author)
             with open(
                     os.path.join(os.path.expanduser('~'), 'Desktop', 'results.txt'), 'a+'
             ) as f:
@@ -35,6 +39,9 @@ def getAllRepositories(stringFolder, language):
 
 def modifiedFilesPerCommit(commit, stringFileName):
     for f in commit.modified_files:
+        if ".xml" or ".fxml" in f.filename:
+            if commit.modified_files != 1 and f.change_type == "Added":
+                return True
         if len(f.changed_methods) == 1:
             return True
         if f.filename == stringFileName:
@@ -57,14 +64,68 @@ def englishCommit(repoString):
         words = message.split(" ")
 
         if words[0].endswith("ing") or words[0].endswith("ed"):
+            print("Wrong1")
+            send -= 0.15
+        if words[0][0].islower():
+            print("Wrong2")
+            send -= 0.15
+        if not words[0] in startingWords:
+            print("Wrong3")
+            send -= 0.25
+            if words[0][0].islower():
+                send += 0.1
+        if words[len(words) - 1].endswith("."):
+            print("Wrong4")
+            send -= 0.1
+        if len(message) >= 50:
+            print("Wrong5")
+            send -= 0.25
+        containsFile = False
+
+        for w in words:
+            if w != "Initial" and w != "Refactor" and w != "Change" and w != "Edit" and w != "Modify":
+                if modifiedFilesPerCommit(commit, w):
+                    containsFile = True
+            else:
+                containsFile = True
+
+        if not containsFile:
+            print("Wrong6")
+            send -= 0.1
+
+        full += send
+        length += 1
+
+    return full / length
+
+
+def bosnianCommit(repoString):
+    full = 0
+    length = 0
+
+    startingWordsBosnian = ["Dodan", "Promjen", "Isprav", "Reorganiz", "Zavrsen", "Inicijaliz",
+                            "Spoj", "Preda", "Obrad", "Ažurira", "Postav", "Pomjer", "Forsir", "Prošir", "Ured",
+                            "Izmijen",
+                            "Refaktor", "Kreira", "Uklon"]
+    commits = Repository(repoString).traverse_commits()
+    for commit in commits:
+        send = 1
+        message = commit.msg
+        words = message.split(" ")
+
+        if words[0].endswith("ing") or words[0].endswith("ed"):
             # print("Wrong1")
             send -= 0.15
         if words[0][0].islower():
             # print("Wrong2")
             send -= 0.15
-        if not words[0] in startingWords:
-            # print("Wrong3")
-            send -= 0.25
+        send -= 0.25
+        search = True
+        for onePart in startingWordsBosnian:
+            if onePart in words[0] or onePart.lower() in words[0]:
+                if onePart == "Inicijaliz" or onePart == "Refaktor" or onePart == "Promjen" or onePart == "Izmijen" or onePart == "Ažurira":
+                    search = False
+                send += 0.25
         if words[len(words) - 1].endswith("."):
             # print("Wrong4")
             send -= 0.1
@@ -74,7 +135,7 @@ def englishCommit(repoString):
         containsFile = False
 
         for w in words:
-            if w != "Initial" and w != "Refactor" and w != "Change" and w != "Edit" and w != "Modify":
+            if search:
                 if modifiedFilesPerCommit(commit, w):
                     containsFile = True
             else:
@@ -120,4 +181,7 @@ def evaluationDMM(repoString):
 
 
 if __name__ == '__main__':
-    getAllRepositories(sys.argv[1], "-e")
+    send = ""
+    if len(sys.argv) == 3:
+        send = sys.argv[2]
+    getAllRepositories(sys.argv[1], send)
